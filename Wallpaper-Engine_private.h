087c9c37be8fbefd56cb7dfe9400ed7c0602bb12
Konohamaru04 <dedsec.konohamaru@gmail.com>
2020-10-12 05:51:43 +0530

    Added browse file feature
    Added browse file feature

---
+++ Wallpaper-Engine_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef WALLPAPER-ENGINE_PRIVATE_H
+#define WALLPAPER-ENGINE_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.1"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	"DedSec"
+#define FILE_VERSION	"1.0.0.1"
+#define FILE_DESCRIPTION	"Wallpaper Engine"
+#define INTERNAL_NAME	"Wallpaper Engine"
+#define LEGAL_COPYRIGHT	"github/Konohamaru04"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Wallpaper Engine"
+#define PRODUCT_NAME	"Wallpaper Engine"
+#define PRODUCT_VERSION	"1.0.0.1"
+
+#endif /*WALLPAPER-ENGINE_PRIVATE_H*/
